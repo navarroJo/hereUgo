332c0d04245c02324d3c9448fb54aa44f15f25bd
navarroJo <joeyn@joeynavarro.com>
2015-02-25 15:25:47 -0800

    Update and rename portBox.js to hereUgo.js

--- js/hereUgo.js
+++ js/hereUgo.js
@@ -1,5 +1,5 @@
 /*
- * 	portBox 1.0 - jQuery plugin
+ * 	hereUgo 1.0 - jQuery plugin
  *	written by Joey Navarro	
  *	http://www.joeynavarro.com
  *
@@ -20,8 +20,8 @@ Listen For Click
 
 $(document).on('click', '[data-display]', function(e){
 	e.preventDefault();
-	var portBoxID = $(this).attr('data-display');
-	$('#'+portBoxID).display($(this).data());	
+	var hereUgoID = $(this).attr('data-display');
+	$('#'+hereUgoID).display($(this).data());	
 
 });
 
@@ -53,9 +53,9 @@ Set Defaults
 Globals Are Nice
 -------------------------*/	
 			
-			var portBox = $(this),
+			var hereUgo = $(this),
 				locked = false,
-				portBoxBG = $('.portBox-overlay'),
+				hereUgoBG = $('.hereUgo-overlay'),
 				scrollBar;
 				
 /*--------------------------
@@ -72,19 +72,19 @@ IE Check
 Animations Locks
 ----------------------------*/
 	
-			function unlockPB() { 
+			function unlockHUG() { 
 				locked = false;
 			}
-			function lockPB() {
+			function lockHUG() {
 				locked = true;
 			}			
 /*------------------------
 Drop in da BG
 -------------------------*/
 			
-			//If portBox background does not exist add it after portBox
-			if(portBoxBG.length == 0) {
-				portBoxBG = $('<div class="portBox-overlay" />').insertAfter(portBox);
+			//If hereUgo background does not exist add it after hereUgo
+			if(hereUgoBG.length == 0) {
+				hereUgoBG = $('<div class="hereUgo-overlay" />').insertAfter(hereUgo);
 			}
 
 /*-------------------------
@@ -99,13 +99,13 @@ Scroll Bar and Close Button
 				scrollBar = false;	
 			};
 
-			//IF portBox does not have a scroll bar div  and scrollBar is set to TRUE
-			if (portBox.has(".scrollBar").length == 0 && scrollBar == true){
+			//IF hereUgo does not have a scroll bar div  and scrollBar is set to TRUE
+			if (hereUgo.has(".scrollBar").length == 0 && scrollBar == true){
 				
-				//Wrap Everything in the portBox div in a new div with class of scrollBar
-				//Add 20 padding on right and set portBox padding to 10 to offset inclusion of scroll bar
-				portBox.wrapInner('<div class="scrollBar" style="padding-right:20px;" />'),
-				portBox.css({'padding-right' : 10});
+				//Wrap Everything in the hereUgo div in a new div with class of scrollBar
+				//Add 20 padding on right and set hereUgo padding to 10 to offset inclusion of scroll bar
+				hereUgo.wrapInner('<div class="scrollBar" style="padding-right:20px;" />'),
+				hereUgo.css({'padding-right' : 10});
 				
 				//Run the slimScroll function
 				$(function(){
@@ -115,106 +115,106 @@ Scroll Bar and Close Button
 				});	
 				
 				//Add Close button anchor tag last so it's not traped in slimScroll div
-				portBox.append('<a class="close-portBox">&#215;</a>');		
+				hereUgo.append('<a class="close-hereUgo">&#215;</a>');		
 			}
 			
 			//If no Close button exists add one
-			if (portBox.has(".close-portBox").length == 0 && scrollBar == false){
+			if (hereUgo.has(".close-hereUgo").length == 0 && scrollBar == false){
 				
-				portBox.append('<a class="close-portBox">&#215;</a>');
+				hereUgo.append('<a class="close-hereUgo">&#215;</a>');
 			}
 			
 /*------------------------
 Keep It In Da Center
 -------------------------*/	
 			
-			portBox.center = function () {
+			hereUgo.center = function () {
 			var top, left;
 			
 				//If window width is greater than or equal to 1025px 
 				//OR box height	is greater than window height 
 				//AND scrollBar is TRUE set height to 90%
-				 if(portBox.outerHeight() + 25 > $(window).height() && scrollBar === true && mobileIE === false){
+				 if(hereUgo.outerHeight() + 25 > $(window).height() && scrollBar === true && mobileIE === false){
 					 
 					 var maxH = $(window).height() - 80;
 					 
-					portBox.css({'height' :maxH + 'px'});	 
+					hereUgo.css({'height' :maxH + 'px'});	 
 				}
 				
 				//Set top an left to 0 <-- seems unnecessary but fixed window offsetting problems when box first appears
-				portBox.css({
+				hereUgo.css({
 					top:0, 
 					left:0
 				});
 			
-				//Get the windows height minus the portBox height divide it in half
-				//Get the windows width minus the portBox width divide it in half
+				//Get the windows height minus the hereUgo height divide it in half
+				//Get the windows width minus the hereUgo width divide it in half
 				
-				top = Math.max($(window).height() - portBox.outerHeight(), 0) / 2;
-				left = Math.max($(window).width() - portBox.outerWidth(), 0) / 2;
+				top = Math.max($(window).height() - hereUgo.outerHeight(), 0) / 2;
+				left = Math.max($(window).width() - hereUgo.outerWidth(), 0) / 2;
 				
-				//Set top and left values for portBox to center it 
+				//Set top and left values for hereUgo to center it 
 				
-				portBox.css({
+				hereUgo.css({
 					top:top + $(window).scrollTop(), 
 					left:left + $(window).scrollLeft()
 				});
-				// If window width greater than or equal to portBox width move close icon inside box 
+				// If window width greater than or equal to hereUgo width move close icon inside box 
 				//and leave 5px margin on either side of box
 				
-				if(portBox.innerWidth() + 10 >= $(window).width()){
-					portBox.css({'margin-right' : 5 + 'px'}),
-					$('.close-portBox').css({'top': 3, 'right': 3});
+				if(hereUgo.innerWidth() + 10 >= $(window).width()){
+					hereUgo.css({'margin-right' : 5 + 'px'}),
+					$('.close-hereUgo').css({'top': 3, 'right': 3});
 					
 				// Else no margin and position close icon upper right hanging off corner		
 				}else{
-					portBox.css({'margin-right' : 0 + 'px'}),
-					$('.close-portBox').css({'top': -6, 'right': -7});	
+					hereUgo.css({'margin-right' : 0 + 'px'}),
+					$('.close-hereUgo').css({'top': -6, 'right': -7});	
 				}
 				
-				//If portBox height is greater than or equal to window height add top of 20 
-				if(portBox.outerHeight() >= $(window).height() && scrollBar == false){
-					portBox.css({'top' : 20 + $(window).scrollTop()});
+				//If hereUgo height is greater than or equal to window height add top of 20 
+				if(hereUgo.outerHeight() >= $(window).height() && scrollBar == false){
+					hereUgo.css({'top' : 20 + $(window).scrollTop()});
 				}
 
 			};
 			
 			//Run the center function
-			portBox.center();	
+			hereUgo.center();	
 			
 			//Run it again as window is resized
-			$(window).on('resize.portBox', portBox.center);
+			$(window).on('resize.hereUgo', hereUgo.center);
 			
 /*------------------------
 Make It Appear 
 -------------------------*/ 
 			
-			//When the portBox opens
-			portBox.on('portBox:open', function () {
+			//When the hereUgo opens
+			hereUgo.on('hereUgo:open', function () {
 				
 				//If its not locked
 				if(!locked) {
 					
 					//lock It
-					lockPB();
+					lockHUG();
 					
 					//If no animation is set
 					if(options.animation == '') {
 						
-						//Set portBox and background to display block so they just appear 
-						portBox.css({'display' : 'block'}),
-						portBoxBG.css({'display':'block'});	
+						//Set hereUgo and background to display block so they just appear 
+						hereUgo.css({'display' : 'block'}),
+						hereUgoBG.css({'display':'block'});	
 						
-						//Then unlock the portBox
-						unlockPB();				
+						//Then unlock the hereUgo
+						unlockHUG();				
 					}else{
 						
-						//Else fade in the BG and run animation on portBox
-						portBoxBG.fadeIn(options.animationspeed);
-						portBox.show(options.animation, options.animationspeed);
+						//Else fade in the BG and run animation on hereUgo
+						hereUgoBG.fadeIn(options.animationspeed);
+						hereUgo.show(options.animation, options.animationspeed);
 						
-						//Then unlock portBox
-						unlockPB();																				
+						//Then unlock hereUgo
+						unlockHUG();																				
 					}
 				}
 			});
@@ -223,37 +223,37 @@ Make It Appear
 Make It Go Away
 -------------------------*/	
 			
-			//When portBox is closed
-			portBox.on('portBox:close', function () {
+			//When hereUgo is closed
+			hereUgo.on('hereUgo:close', function () {
 				
 				//If its not locked
 				if(!locked) {
 					
 					//lock It
-					lockPB();
+					lockHUG();
 					
 					//If no animation is set
 					if(options.animation == '') {
 						
-						//Set portBox and background back to display none  
-						portBox.css({'display' : 'none'}),
-						portBoxBG.css({'display' : 'none'});
+						//Set hereUgo and background back to display none  
+						hereUgo.css({'display' : 'none'}),
+						hereUgoBG.css({'display' : 'none'});
 						
-						//Then unlock portBox
-						unlockPB();	
+						//Then unlock hereUgo
+						unlockHUG();	
 					}else{
 						
-						//Else fade out the BG and portBox
-						portBoxBG.fadeOut(options.animationspeed),
-						portBox.fadeOut(options.animationspeed);
+						//Else fade out the BG and hereUgo
+						hereUgoBG.fadeOut(options.animationspeed),
+						hereUgo.fadeOut(options.animationspeed);
 						
-						//Then unlock portBox
-						unlockPB();				
+						//Then unlock hereUgo
+						unlockHUG();				
 					} 
 					
 					//On close turn off the resize handler 
-					//triggering the portBox.center function  		
-					$(window).off('resize.portBox');	
+					//triggering the hereUgo.center function  		
+					$(window).off('resize.hereUgo');	
 				}}
 			);     
 
@@ -262,23 +262,23 @@ Open and Closing Listeners
 --------------------------------*/
 			
 			//Open 
-			portBox.trigger('portBox:open')
+			hereUgo.trigger('hereUgo:open')
 			
 			//Close
 			//Close Button
-			var closeButton = $('.close-portBox').one('click.portBoxEvent', function () {
-				portBox.trigger('portBox:close')
+			var closeButton = $('.close-hereUgo').one('click.hereUgoEvent', function () {
+				hereUgo.trigger('hereUgo:close')
 			});
 			//Background Click
 			if(options.bgclose) {
-				portBoxBG.css({'cursor':'pointer'})
-				portBoxBG.one('click.portBoxEvent', function () {
-				portBox.trigger('portBox:close')
+				hereUgoBG.css({'cursor':'pointer'})
+				hereUgoBG.one('click.hereUgoEvent', function () {
+				hereUgo.trigger('hereUgo:close')
 				});
 			}
 			//Escape Key
 			$('body').keyup(function(e) {
-				if(e.which===27){ portBox.trigger('portBox:close'); }
+				if(e.which===27){ hereUgo.trigger('hereUgo:close'); }
 			});			
 		
 		});//end return each function 
